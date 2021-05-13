24ee9fc640fb7dfd0fb70f6afac2ed46bba6251e
ANSEONGHEE <84111487+ANSEONGHEE@users.noreply.github.com>
2021-05-13 11:53:37 +0900

    두번째 연습

--- dd.java
+++ dd.java
@@ -2,11 +2,14 @@ package dd;
 
 public class dd {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		int i = 1;
 		
 				
 		while(i < 10)
+while(i < 10)
+while(i < 10)
+
 		
 		{
 			System.out.println("3*" + i + "=" + (3*i));

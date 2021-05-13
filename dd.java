5b6a9b86d16cbe03befadce60000318a8d9909f7
ANSEONGHEE <84111487+ANSEONGHEE@users.noreply.github.com>
2021-05-13 11:52:29 +0900

    연습1

---
+++ dd.java
@@ -0,0 +1,19 @@
+package dd;
+
+public class dd {
+
+	public static void main(String[] args) {
+		int i = 1;
+		
+				
+		while(i < 10)
+		
+		{
+			System.out.println("3*" + i + "=" + (3*i));
+			i = i+1;
+			
+		}
+
+	}
+
+}
